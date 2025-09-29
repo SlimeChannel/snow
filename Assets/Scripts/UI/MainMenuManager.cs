90fe0de9393a01c52f09dc22fbfa15825347dad9
ScLfMI <inkizhinov.maksim@ya.ru>
2025-09-29 17:53:18 +0700

    menu with mouse + keyboard navigation

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,28 +1,70 @@
 namespace snow.UI
 {
     using UnityEngine;
+    using UnityEngine.UI;
     using UnityEngine.EventSystems;
+    using System.Collections.Generic;
+
     public class MainMenuManager : MonoBehaviour
     {
         [SerializeField] private Animator _anim;
-        [SerializeField] private Transform SettingsContainer;
+        [SerializeField] private Transform _settingsContainer;
+        [SerializeField] private GameObject _defaultSelectable;
+        [SerializeField] private RectTransform _settingsArrow;
+        private Dictionary<string, int> _arrowPositions = new()
+        {
+            {"game", 50},
+            {"graphics", -50},
+            {"audio", -150},
+            {"controls", -250}
+        };
+        private void Update()
+        {
+            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+                PointerEventData pointerData = new PointerEventData(EventSystem.current);
+                pointerData.position = Input.mousePosition;
+
+                System.Collections.Generic.List<RaycastResult> results = new System.Collections.Generic.List<RaycastResult>();
+                EventSystem.current.RaycastAll(pointerData, results);
+
+                foreach (var result in results)
+                {
+                    Button button = result.gameObject.GetComponentInParent<Button>();
+                    if (button != null && button.interactable)
+                    {
+                        EventSystem.current.SetSelectedGameObject(button.gameObject);
+                        break;
+                    }
+                    else if (result.gameObject.name == "Background")
+                        break;
+                }
+            }
+            if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
+                if (EventSystem.current.currentSelectedGameObject == null)
+                    EventSystem.current.SetSelectedGameObject(_defaultSelectable);
+        }
         public void TabChange(string animationName)
         {
             _anim.Play(animationName);
+        }
+        public void TabSelect(GameObject defaultSelectable)
+        {
             EventSystem.current.SetSelectedGameObject(null);
+            _defaultSelectable = defaultSelectable;
         }
         public void ExecuteExit()
         {
-
+            Application.Quit();
         }
         public void SelectSettingsCategory(string category)
         {
-            for (int i = 0; i < SettingsContainer.childCount; i++)
+            for (int i = 0; i < _settingsContainer.childCount; i++)
             {
-                Debug.Log(i);
-                GameObject child = SettingsContainer.GetChild(i).gameObject;
-                child.SetActive(tag == child.GetComponent<SettingsTab>().Tag);
+                GameObject child = _settingsContainer.GetChild(i).gameObject;
+                child.SetActive(category == child.GetComponent<SettingsTab>().Tag);
             }
+            _settingsArrow.anchoredPosition = new Vector2(_settingsArrow.anchoredPosition.x, _arrowPositions[category]);
         }
         public void SaveSettings()
         {
